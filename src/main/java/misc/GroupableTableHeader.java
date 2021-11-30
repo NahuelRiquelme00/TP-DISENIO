e7c8a62fac65f0e495f784ee4f74d57566205a19
FedericoPacheco <fedepacheco2112@gmail.com>
2021-11-30 19:46:29 -0300

    Ancho columnas solucionado

--- src/main/java/misc/GroupableTableHeader.java
+++ src/main/java/misc/GroupableTableHeader.java
@@ -19,51 +19,64 @@ import javax.swing.table.*;
   * @author Nobuo Tamemasa
   */
 
-public class GroupableTableHeader extends JTableHeader {
-  private static final String uiClassID = "GroupableTableHeaderUI";
-  protected Vector columnGroups = null;
-    
-  public GroupableTableHeader(TableColumnModel model) {
-    super(model);
-    setUI(new GroupableTableHeaderUI());
-    setReorderingAllowed(false);
-  }
-  public void updateUI(){
-   setUI(new GroupableTableHeaderUI());
-  }
-  
-  public void setReorderingAllowed(boolean b) {
-    reorderingAllowed = false;
-  }
-    
-  public void addColumnGroup(ColumnGroup g) {
-    if (columnGroups == null) {
-      columnGroups = new Vector();
-    }
-    columnGroups.addElement(g);
-  }
+public class GroupableTableHeader extends JTableHeader 
+{
+    private static final String uiClassID = "GroupableTableHeaderUI";
+    protected Vector columnGroups = null;
 
-  public Enumeration getColumnGroups(TableColumn col) {
-    if (columnGroups == null) return null;
-    Enumeration e = columnGroups.elements();
-    while (e.hasMoreElements()) {
-      ColumnGroup cGroup = (ColumnGroup)e.nextElement();
-      Vector v_ret = (Vector)cGroup.getColumnGroups(col,new Vector());
-      if (v_ret != null) { 
-  return v_ret.elements();
-      }
+    public GroupableTableHeader(TableColumnModel model) 
+    {
+        super(model);
+        setUI(new GroupableTableHeaderUI());
+        setReorderingAllowed(false);
     }
-    return null;
-  }
+    
+    public void updateUI()
+    {
+        setUI(new GroupableTableHeaderUI());
+        /*
+        TableCellRenderer renderer = getDefaultRenderer();
+            if (renderer instanceof Component) {
+                SwingUtilities.updateComponentTreeUI((Component)renderer);
+          }
+        */
+    }
   
-  public void setColumnMargin() {
-    if (columnGroups == null) return;
-    int columnMargin = getColumnModel().getColumnMargin();
-    Enumeration e = columnGroups.elements();
-    while (e.hasMoreElements()) {
-      ColumnGroup cGroup = (ColumnGroup)e.nextElement();
-      cGroup.setColumnMargin(columnMargin);
+    public void setReorderingAllowed(boolean b) {
+        reorderingAllowed = false;
+    }
+
+    public void addColumnGroup(ColumnGroup g) 
+    {
+        if (columnGroups == null) {
+            columnGroups = new Vector();
+        }
+        columnGroups.addElement(g);
+    }
+
+    public Enumeration getColumnGroups(TableColumn col) 
+    {
+        if (columnGroups == null) return null;
+        Enumeration e = columnGroups.elements();
+        while (e.hasMoreElements()) {
+            ColumnGroup cGroup = (ColumnGroup)e.nextElement();
+            Vector v_ret = (Vector)cGroup.getColumnGroups(col,new Vector());
+            if (v_ret != null) { 
+                return v_ret.elements();
+            }
+        }
+        return null;
+    }
+
+    public void setColumnMargin() // Pasa los margenes a los encabezados
+    {                               
+        if (columnGroups == null) return;
+        int columnMargin = getColumnModel().getColumnMargin();
+        Enumeration e = columnGroups.elements();
+        while (e.hasMoreElements()) 
+        {
+            ColumnGroup cGroup = (ColumnGroup)e.nextElement();
+            cGroup.setColumnMargin(columnMargin);
+        }
     }
   }
-  
-}
