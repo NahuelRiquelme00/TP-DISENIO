dae3087fd50b68907bc2197dfd4fec95e86da045
FedericoPacheco <fedepacheco2112@gmail.com>
2021-12-01 19:19:06 -0300

    Re-coloreado grilla cuando se ocupa / reserva

--- src/main/java/interfaces/mostrarEstadoHabitacion/TablaColoreada.java
+++ src/main/java/interfaces/mostrarEstadoHabitacion/TablaColoreada.java
@@ -4,12 +4,12 @@
  */
 package interfaces.mostrarEstadoHabitacion;
 
+import entidades.TipoEstado;
 import java.awt.Color;
 import java.awt.Component;
 import javax.swing.JTable;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableCellRenderer;
-import interfaces.mostrarEstadoHabitacion.GroupableTableHeader;
 
 /**
  *
@@ -31,9 +31,10 @@ public class TablaColoreada extends JTable
         Component celda = super.prepareRenderer(rend, rowInd, colInd);
         Object val = getValueAt(rowInd, colInd);
         
-        if (val != null) //&& colInd > 0) 
+        if (val != null)
         {
             auxColor = (Color) val;
+            
             // https://stackoverflow.com/a/20472101
             if (this.isRowSelected(rowInd) && this.isColumnSelected(colInd))
                 // https://docs.oracle.com/javase/7/docs/api/java/awt/Color.html#darker()
@@ -54,5 +55,28 @@ public class TablaColoreada extends JTable
     @Override
     protected JTableHeader createDefaultTableHeader() {
         return new GroupableTableHeader(columnModel);
+    }
+
+    public static Color getColorGrilla(TipoEstado est) {
+        Color res;
+        switch (est) {
+            case DISPONIBLE:
+                res = TablaColoreada.COLOR_DISPONIBLE;
+                break;
+            case RESERVADA:
+                res = TablaColoreada.COLOR_RESERVADA;
+                break;
+            case OCUPADA:
+                res = TablaColoreada.COLOR_OCUPADA;
+                break;
+            case FUERA_DE_SERVICIO:
+                res = TablaColoreada.COLOR_FUERA_DE_SERVICIO;
+                break;
+            default:
+                // No se deberia llegar aca
+                res = TablaColoreada.COLOR_ERROR;
+                break;
+        }
+        return res;
     }
 }
